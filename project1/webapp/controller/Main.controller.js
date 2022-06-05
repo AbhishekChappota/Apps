79f3bdbf574d7e75147e7d68022c5d602a0e107a
Abhishek Malani <abhishekmalanimalani@gmail.com>
2022-06-05 05:37:09 +0000

    Main file changed

--- project1/webapp/controller/Main.controller.js
+++ project1/webapp/controller/Main.controller.js
@@ -9,7 +9,7 @@ sap.ui.define([
 
         return Controller.extend("project1.controller.Main", {
             onInit: function () {
-
+                    console.log("Abhishek");
             }
         });
     });
